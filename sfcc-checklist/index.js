1c256a3c4996884a3882bc979f71847eb6a8a969
Peter Regala <darking.31.pr@gmail.com>
2026-02-25 15:19:39 +0800

    initialize

---
+++ sfcc-checklist/index.js
@@ -0,0 +1,30 @@
+
+const btn = document.querySelector('.takeImageBtn');
+
+
+btn.addEventListener('click', () => {
+	html2canvas(document.querySelector('.template_checklist_div'))
+	.then(canvas => {
+		const url = canvas.toDataURL('image/png');
+		const a = document.createElement('a');
+		a.setAttribute('download', 'Checklist.png');
+		a.setAttribute('href',url);
+		a.click();
+	})
+})
+
+btn.addEventListener('click', () => {
+	html2canvas(document.querySelector('.general_checklist_div'))
+	.then(canvas => {
+		const url = canvas.toDataURL('image/png');
+		const a = document.createElement('a');
+		a.setAttribute('download', 'Checklist.png');
+		a.setAttribute('href',url);
+		a.click();
+	})
+})
+
+
+
+
+
